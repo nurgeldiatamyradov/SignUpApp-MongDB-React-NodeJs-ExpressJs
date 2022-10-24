9ce9c150f070aa53f297715e6cc85def215fbf07
nurgeldi atamyradov <67277491+nurgeldiatamyradov@users.noreply.github.com>
2022-10-24 12:58:29 +0200

    done

---
+++ signupbackend/routes/routes.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+const signUpTemplateCopy = require('../models/SignUpModels')
+
+
+router.post('/signup', (request, response) => {
+    
+
+
+    const signUpUser = new signUpTemplateCopy({
+        fullName:request.body.fullName,
+        userName:request.body.username,
+        email:request.body.email,
+        password:request.body.password
+    })
+    signUpUser.save()
+    .then(data=> {
+        response.json(data)
+    })
+    .catch(error =>{
+        response.json(error)
+    })
+})
+
+
+
+module.exports = router   
